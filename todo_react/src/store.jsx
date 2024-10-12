dbdd6939f2387d828aec1d4428d1c30449498550
devhaeun <lala819010@gmail.com>
2024-10-12 23:19:01 +0900

    redux toolkit: createAction, createReducer 사용

--- todo_react/src/store.jsx
+++ todo_react/src/store.jsx
@@ -1,33 +1,19 @@
 import {createStore} from 'redux';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DEL = 'DEL';
-
-const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    };
-};
-
-const deleteToDo = (id) => {
-    return {
-        type: DEL,
-        id,
-    };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DEL");
 
 // reducer
-const reducer = (state=[], action) => {
-    switch(action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DEL:
-            return state.filter(toDo => toDo.id !== action.id);
-        default:
-            return state;
-    };
-};
+const reducer = createReducer([], (builder) => {
+    builder
+    .addCase(addToDo, (state, action) => {
+        state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) =>
+        state.filter(toDo => toDo.id !== action.payload)
+    )
+});
 
 // store
 const store = createStore(reducer);
